2186203513c3004f1c9dc3fa509e86730d98a6e0
Michael Trang <michael.trang@hotmail.com>
2018-10-12 20:42:57 +1100

    add graphql server playground

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
-const { ApolloServer } = require('apollo-server')
+// import express from 'express'
+// import { MongoClient } from 'mongodb'
+// import { readFileSync } from 'fs'
+// import expressPlayground from 'graphql-playground-middleware-express'
+const { ApolloServer } = require('apollo-server-express')
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const { readFileSync } = require('fs')
+const expressPlayground = require('graphql-playground-middleware-express').default
 
-const typeDefs = `
-  type Query {
-    totalPhotos: Int!
-  }
-`
+const resolvers = require('./resolvers')
 
-const resolvers = {
-  Query: {
-    totalPhotos: () => 88
+require('dotenv').config()
+
+const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
+
+async function start() {
+  const app = express()
+  const MONGO_DB = process.env.DB_HOST
+  let db
+  
+  try {
+    const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true })
+    db = client.db()
+  } catch (error) {
+    console.log(`
+    Mongo DB Host not found!
+    please add DB_HOST environment variable to .env file
+
+    exiting...
+    `)
+    process.exit(1)
   }
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization
+      const currentUser = await db.collection('users').findOne({ githubToken })
+      return { db, currentUser }
+    }
+  })
+
+  server.applyMiddleware({ app })
+
+  app.get('/playgournd', expressPlayground({ endpoint: '/graphql'}))
+  
+  app.get('/', (req, res) => {
+    const url = `https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID}&scope=user`
+    res.end(`<a href="${url}">Sign In with Github</a>`)
+  })
+
+  app.listen(process.env.PORT, () => console.log(`GraphQL Server is running at ${process.env.HOST}:${process.env.PORT}${server.graphqlPath}`))
 }
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
-
-server.listen()
-  .then(({url}) => console.log(`GraphQL Service running on ${url}`))
+start()
---
+++ resolvers/Mutation.js
@@ -0,0 +1,81 @@
+const { authorizeWithGithub } = require('../lib')
+const fetch = require('node-fetch')
+const { ObjectID } = require('mongodb')
+
+module.exports = {
+
+  async postPhoto(parent, args, { db, currentUser }) {
+    if (!currentUser) {
+      throw new Error('only authorized user can post a photo')
+    }
+    const newPhoto = {
+      ...args.input,
+      userID: currentUser.githubLogin,
+      created: new Date()
+    }
+    const { insertedIds } = await db.collection('photos').insert(newPhoto)
+    newPhoto.id = insertedIds[0]
+
+    return newPhoto
+  },
+
+  async tagPhoto(parent, args, { db }) {
+    await db.collection('tags').replaceOne(args, args, { upsert: true})
+    
+    return db.collection('photos').findOne({ _id: ObjectID(args.photoID) })
+  },
+
+  async githubAuth(parent, { code }, { db }) {
+    const {
+      message,
+      access_token,
+      avatar_url,
+      login,
+      name
+    } = await authorizeWithGithub({
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code
+    })
+    if (message) {
+      throw new Error(message)
+    }
+    const latestUserInfo = {
+      name,
+      githubLogin: login,
+      githubToken: access_token,
+      avatar: avatar_url
+    }
+    const { ops:[user] } = await db
+      .collection('users')
+      .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true})
+
+    return { user, token: access_token }
+  },
+
+  async addFakeUsers(parent, { count }, { db }) {
+    const randomUserApi = `https://randomuser.me/api/?results=${count}`
+    const { results } = await fetch(randomUserApi).then(res => res.json())
+    const users = results.map(r => ({
+      githubLogin: r.login.username,
+      name: `${r.name.first} ${r.name.last}`,
+      avatar: r.picture.thumbnail,
+      githubToken: r.login.sha1
+    }))
+    await db.collection('users').insert(users)
+
+    return users
+  },
+
+  async fakeUserAuth(parent, { githubLogin }, { db }) {
+    const user = await db.collection(usres).findOne({ githubLogin })
+    if (!user) {
+      throw new Error(`Cannot find user with githubLogin "${githubLogin}"`)
+    }
+
+    return {
+      token: user.githubToken,
+      user
+    }
+  }
+}
